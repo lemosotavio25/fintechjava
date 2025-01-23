0c7a44074ed69bbed73f597abbd26725b62048ff
lemosotavio25 <113394883+lemosotavio25@users.noreply.github.com>
2025-01-22 23:11:46 -0300

    Update ConnectionManager.java

--- src/main/java/br/com/fiap/fintech/dao/ConnectionManager.java
+++ src/main/java/br/com/fiap/fintech/dao/ConnectionManager.java
@@ -25,8 +25,8 @@ public class ConnectionManager {
 
             connection = DriverManager.getConnection(
                     "jdbc:oracle:thin:@oracle.fiap.com.br:1521:ORCL",
-                    "rm558425",
-                    "250794");
+                    "",
+                    "");
 
         } catch (Exception e) {
             e.printStackTrace();
