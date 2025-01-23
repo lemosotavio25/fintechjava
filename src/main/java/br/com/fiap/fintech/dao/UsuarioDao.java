fcdd98e3e9e2cdce1ac1d78dbc48df133d757a2e
lemosotavio25 <113394883+lemosotavio25@users.noreply.github.com>
2025-01-22 23:24:02 -0300

    Update UsuarioDao.java

--- src/main/java/br/com/fiap/fintech/dao/UsuarioDao.java
+++ src/main/java/br/com/fiap/fintech/dao/UsuarioDao.java
@@ -4,7 +4,6 @@ import br.com.fiap.fintech.exception.DBException;
 import br.com.fiap.fintech.model.Usuario;
 
 public interface UsuarioDao {
-
     boolean validarUsuario(Usuario usuario) throws DBException;;
     void cadastrar(Usuario usuario) throws DBException;
     Usuario buscarPorEmail(String email) throws DBException;
